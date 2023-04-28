1a8bbe458cfbc19f189b442b3aa20495bd8614f5
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-04-29 00:42:55 +0200

    Doc : Commented tuple

--- Tuple.java
+++ Tuple.java
@@ -1,17 +1,41 @@
-
+/**
+ * Tuple to handle pairs of int more easily
+ */
 class Tuple {
     public int x;
     public int y;
+
+    /**
+     * Constructor of a tuple
+     * @param o first int
+     * @param t second int
+     */
     public Tuple(int o, int t){
         x = o;
         y = t;
     }
+
+    /**
+     * Create a copy of this tuple and returns it
+     * @return a copy of this tuple
+     */
     public Tuple copy(){
         return new Tuple(x, y);
     }
+
+    /**
+     * Returns a String version of this tuple
+     * @return a representation of this as a Tuple
+     */
     public String toString(){
         return "["+x+";"+y+"]";
     }
+
+    /**
+     * Checks if another Tuple is equal to this
+     * @param obj the tuple to compare
+     * @return true if they contain the same values
+     */
 
     public boolean equals(Object obj){
         if (obj == this) {
